de7de32b69d1cdca77c3fde9b087e45ea4a8b500
square.unit <square.unit@yandex.ru>
2019-12-04 18:38:18 +0300

    beginning

--- FunWithArrays/Program.cs
+++ FunWithArrays/Program.cs
@@ -6,7 +6,28 @@ namespace FunWithArrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Arrays *****");
+            SimpleArrays();
+            Console.ReadLine();
+        }
+
+        static void SimpleArrays()
+        {
+            Console.WriteLine("=> Simple Array Creation.");
+            //Create an array of ints containing 3 elements indexed 0, 1, 2
+            int[] myInts = new int[3];
+            myInts[0] = 100;
+            myInts[1] = 200;
+            myInts[2] = 300;
+
+            //Now print each value.
+            foreach(int i in myInts)
+                Console.WriteLine(i);
+            Console.WriteLine();
+
+            //Create a 100 item string array, indexed 0 - 99
+            string[] booksOnDotNet = new string[100];
+            Console.WriteLine();
         }
     }
 }
